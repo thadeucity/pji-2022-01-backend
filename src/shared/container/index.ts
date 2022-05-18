ffab1d714d07756a58522a51e8ebb2d0bcf53140
Victor Alvarenga <info@victoralvarenga.com>
2022-05-18 00:08:33 -0300

    Chore: Create companies repository container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,3 +1,11 @@
 import { container } from 'tsyringe';
 
 import '@providers/index';
+
+import { ICompaniesRepository } from '@modules/companies/repositories/ICompaniesRepository';
+import { CompaniesRepository } from '@modules/companies/repositories/prismaOrm/CompaniesRepository';
+
+container.registerSingleton<ICompaniesRepository>(
+  'CompaniesRepository',
+  CompaniesRepository,
+);
