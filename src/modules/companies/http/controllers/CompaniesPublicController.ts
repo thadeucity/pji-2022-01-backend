a29552749330a106fa5193ec408f7280a4c3c239
Victor Alvarenga <info@victoralvarenga.com>
2022-05-17 22:06:01 -0300

    Chore: Create companies public controller

---
+++ src/modules/companies/http/controllers/CompaniesPublicController.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export class CompaniesPublicController {
+  public async readByUrl(req: Request, res: Response): Promise<Response> {
+    const { url } = req.params || {};
+
+    console.log({url, pr: req.params, req: req.body});
+
+    const company = await prisma.company.findUnique({
+      where: {
+        url: url
+      }
+    })
+
+    if (!company) {
+      return res.status(404).json({
+        message: 'company not found'
+      })
+    }
+
+    return res.json({
+      name: company.name,
+      email: company.email,
+      logo: company.logo,
+      phone: company.phone,
+      primaryColor: company.primary_color,
+      secondaryColor: company.secondary_color
+    })
+  }
+}
